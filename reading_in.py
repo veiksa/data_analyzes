e44d8c6e291b3e46110e421d949d96d46c5332be
Veiko Voolaid <vvoolaid@gmail.com>
2013-09-12 09:07:19 +0200

    file import and duplicate delete function

--- reading_in.py
+++ reading_in.py
@@ -1,44 +1,41 @@
-import pandas as pd
+#import pandas as pd
 import numpy as np
 
 
-###reading the file in
-# faili teekond peab algama kaustast, kus sa praegu asud, muidu python ei suuda seda leida; turvalisuse t6ttu.
+###reading the file in and deleting duplicates
 
-np.genfromtxt
+#The path of the file has to start from 'current' directory
+def file_import(file_path_string, output_name):
 # dtype=None t2hendab, et v6etakse ka mitte numbrilised v22rtused
-isolaadid = np.genfromtxt('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/ZB_OD.csv', dtype=None, delimiter=',', names=True)
+    output_name = np.genfromtxt(file_path_string, dtype=None, delimiter=',', names=True)
 
-isolaadid2 = np.genfromtxt('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/ZB_OD.csv', dtype=None, delimiter=',', names=True)
+###find duplicates
+    names=[]
+    doble=[]
+    for isolate_names in output_name:
+        names.append(isolate_names[0])
 
-#print isolaadid
-###get rid of duplicates
-nimed=[]
-doble=[]
-for andmed in isolaadid:
-    nimed.append(andmed[0])
+    for index, name in enumerate(names):
+        if names.count(name) > 1:
+            doble.append((index,name))
 
-for index, nimi in enumerate(nimed):
-    if nimed.count(nimi) > 1:
-        doble.append((index,nimi))
-print 'Topelt kirjed arrays isolaadid', doble
+#delete duplicates in imported files
+    i=0
+    for x in doble:
+        output_name = np.delete(output_name, x[0]+i, axis=0)
+        i = i-1
+#output_name2 = np.delete(output_name2, (277), axis=0)
+    return output_name
 
+zb = file_import('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/ZB_OD.csv', 'ZB')
 
-i=0
-for x in doble:
-    isolaadid2 = np.delete(isolaadid2, x[0]+i, axis=0)
-    i = i-1
-#isolaadid2 = np.delete(isolaadid2, (277), axis=0)
-print isolaadid2[277]
-
-
-nimed2=[]
-doble2=[]
-for andmed in isolaadid2:
-    nimed2.append(andmed[0])
-
-for index, nimi in enumerate(nimed2):
-    if nimed2.count(nimi) > 1:
-        doble2.append((index,nimi))
-print 'Topelt kirjed arrays isolaadid2', doble2
-print len(isolaadid2), len(isolaadid)
+#nimed2=[]
+#doble2=[]
+#for andmed in output_name2:
+#    nimed2.append(andmed[0])
+#
+#for index, nimi in enumerate(nimed2):
+#    if nimed2.count(nimi) > 1:
+#        doble2.append((index,nimi))
+#print 'Topelt kirjed arrays output_name2', doble2
+#print len(output_name2), len(output_name)
