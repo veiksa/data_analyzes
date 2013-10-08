5766a431e695db9b9e6647379a70b6899568c591
Veiko Voolaid <vvoolaid@gmail.com>
2013-10-08 16:10:16 +0200

    Added lines to convert 1D array to 2D array

--- reading_in.py
+++ reading_in.py
@@ -1,4 +1,3 @@
-#import pandas as pd
 import numpy as np
 
 
@@ -7,7 +6,9 @@ import numpy as np
 #The path of the file has to start from 'current' directory
 def file_import(file_path_string, output_name):
 # dtype=None t2hendab, et v6etakse ka mitte numbrilised v22rtused
-    output_name = np.genfromtxt(file_path_string, dtype=None, delimiter=',', names=True)
+    columns = ['isolaadid','1','2','3','4','5','6','7']   
+    output_name = np.genfromtxt(file_path_string, skip_header=1, dtype=None, delimiter=',', names=columns)
+    #print output_name.shape
 
 ###find duplicates
     names=[]
@@ -18,24 +19,27 @@ def file_import(file_path_string, output_name):
     for index, name in enumerate(names):
         if names.count(name) > 1:
             doble.append((index,name))
-
+    #print doble
 #delete duplicates in imported files
     i=0
     for x in doble:
         output_name = np.delete(output_name, x[0]+i, axis=0)
         i = i-1
 #output_name2 = np.delete(output_name2, (277), axis=0)
-    return output_name
+    isolates=[]
+    isolates = np.append(isolates,output_name['isolaadid'])
+    output_name = np.column_stack((output_name[columns[1]],output_name[columns[2]], output_name[columns[3]], output_name[columns[4]],output_name[columns[5]],output_name[columns[6]],output_name[columns[7]]))
+###Output is a duple that has two numpy arrays in it. One is the isolate names and the other is OD values.
+# The only way that I managed to make 1D array into a 2D array. I think it is easier to work with a 2D array.   
+    return output_name, isolates 
 
-zb = file_import('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/ZB_OD.csv', 'ZB')
+zb = file_import('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/ZB_OD.csv', 'ZB')[0]
+amp100 = file_import('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/amp100.csv', 'amp100')[0]
+isolates = file_import('/home/lubuntu/pythonkursus/data_analyzes/example/andmefailid/ZB_OD.csv', 'ZB')[1]
+print isolates
 
-#nimed2=[]
-#doble2=[]
-#for andmed in output_name2:
-#    nimed2.append(andmed[0])
-#
-#for index, nimi in enumerate(nimed2):
-#    if nimed2.count(nimi) > 1:
-#        doble2.append((index,nimi))
-#print 'Topelt kirjed arrays output_name2', doble2
-#print len(output_name2), len(output_name)
+#isolates = []
+#isolates = np.append(isolates,zb['isolaadid'])
+#newzb = np.column_stack((zb['1'],zb['2'], zb['3'], zb['4'],zb['5'],zb['6'],zb['7']))
+#print newzb[:10], newzb.shape, type(newzb)
+#print isolates[:3],isolates.shape, type(isolates)
---
+++ reading_in.py~
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+amp100 = np.genfromtxt('/home/veiko/hg/data_analyzes/example/andmefailid/AMP100_OD.csv', delimiter=',', names=True)
+
+np.genfromtxt
+
+isolaadid = np.genfromtxt('/home/veiko/hg/data_analyzes/example/andmefailid/AMP100_OD.csv', dtype=None, delimiter=',', names=True)
+
+print isolaadid
